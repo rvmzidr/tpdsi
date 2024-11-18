9ac7a2d7255371ac75abdadcefe3006c5c7817ff
ramzi <dridir844@gmail.com>
2024-11-18 20:11:57 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { ConnexionComponent } from './connexion/connexion.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatToolbarModule,AcceuilComponent,InscriptionComponent,ConnexionComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+path : 'acceuil',
+component : AcceuilComponent                                                                                
+},{
+path : 'connexion',
+component : ConnexionComponent                                                                                            
+},{
+  path : 'inscription',
+  component : InscriptionComponent                                                                                                  
+}
+
+                                                                               
+];
